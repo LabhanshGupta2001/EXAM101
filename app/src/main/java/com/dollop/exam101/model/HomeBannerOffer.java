ca3bdf33d5c9130f053099c5f232351d01c45974
Sutraone <Sutraoone@gmail.com>
2022-06-23 10:28:08 +0530

    23-06-2022_10:21_Guru_Patidar,design half homefragment top package to current affairs

---
+++ app/src/main/java/com/dollop/exam101/model/HomeBannerOffer.java
@@ -0,0 +1,12 @@
+package com.dollop.exam101.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class HomeBannerOffer {
+
+ public int bannerImage;
+    public HomeBannerOffer(int bannerImage) {
+        this.bannerImage = bannerImage;
+    }
+}
